9d88ec3f412554a9a015e6443308e9cf0ab47ba2
SodiqAbdullah <148208065+SodiqAbdullah@users.noreply.github.com>
2023-10-17 17:40:24 +0700

    Add files via upload

---
+++ Pertemuan 1/balok.py
@@ -0,0 +1,6 @@
+panjang = 10
+lebar = 20
+tinggi = 5
+luas = 2 * (panjang*lebar + lebar*tinggi + panjang*tinggi)
+
+print(' panjang = ',panjang,' \n lebar = ',lebar,' \n tinggi = ',tinggi,' \n luas dari balok di atas adalah = ', luas)
