04ef1e5329978d9e02a9ed148f1f7d99b60e9b1b
SodiqAbdullah <148208065+SodiqAbdullah@users.noreply.github.com>
2023-10-18 13:27:27 +0700

    Update kubus.py

--- Pertemuan 1/kubus.py
+++ Pertemuan 1/kubus.py
@@ -1,5 +1,7 @@
 rusuk = 10
+LK = 6*rusuk**2
+LV = rusuk**3
 
-print('hasil dari mencari luas kubus dengan rusuk ', rusuk, ' adalah = ', 6*rusuk**2,)
+print('hasil dari mencari luas kubus dengan rusuk ', rusuk, ' adalah = ', LK)
 
-print('\nhasil dari mencari volume kubus dengan rusuk ', rusuk, ' adalah = ', rusuk**3)
+print('\nhasil dari mencari volume kubus dengan rusuk ', rusuk, ' adalah = ', LV)
