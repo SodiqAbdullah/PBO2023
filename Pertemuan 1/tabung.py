2d8299d0e5b6494be4e104bb0561490774b8797d
SodiqAbdullah <148208065+SodiqAbdullah@users.noreply.github.com>
2023-10-18 14:40:30 +0700

    Create tabung.py

---
+++ Pertemuan 1/tabung.py
@@ -0,0 +1,13 @@
+import math
+jari_jari = 5
+tinggi = 10
+
+# Fungsi untuk menghitung luas permukaan tabung
+luas_permukaan = 2 * math.pi * jari_jari * (jari_jari + tinggi)
+
+# Fungsi untuk menghitung volume tabung
+volume = math.pi * jari_jari**2 * tinggi
+
+print('hasil dari mencari luas tabung dengan\njari_jari : ',jari_jari,'\ntinggi : ',tinggi,'\nadalah = ', luas_permukaan )
+
+print('\nhasil dari mencari volume tabung dengan\njari_jari : ',jari_jari,'\ntinggi : ',tinggi,'\nadalah = ', volume )
