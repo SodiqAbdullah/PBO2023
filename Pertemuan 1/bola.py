5f202ccef9398b03728164663bf00d04a844a668
SodiqAbdullah <148208065+SodiqAbdullah@users.noreply.github.com>
2023-10-18 14:49:33 +0700

    Create bola.py

---
+++ Pertemuan 1/bola.py
@@ -0,0 +1,12 @@
+import math
+jari_jari = 5
+
+# menghitung luas permukaan bola
+luas_permukaan = 4 * math.pi * jari_jari**2
+
+# menghitung volume bola
+volume = (4/3) * math.pi * jari_jari**3
+
+print('hasil dari mencari luas bola dengan jari_jari ', jari_jari, ' adalah = ', luas_permukaan)
+
+print('\nhasil dari mencari volume bola dengan jari_jari ', jari_jari, ' adalah = ', volume)
